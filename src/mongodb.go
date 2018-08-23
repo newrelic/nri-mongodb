781876081f85304217db47871b5c068cd9e3ef61
Camden Cheek <camden.cheek@bluemedora.com>
2018-08-23 12:18:12 -0400

    Added arguments validation

--- src/mongodb.go
+++ src/mongodb.go
@@ -15,7 +15,7 @@ type argumentList struct {
 	Password              string `default:"" help:"Password for the MongoDB connection"`
 	Host                  string `default:"" help:"MongoDB host to connect to for monitoring"`
 	Port                  string `default:"27017" help:"Port on which MongoDB is running"`
-	AuthSource            string `default:"" help:"Database to authenticate against"`
+	AuthSource            string `default:"admin" help:"Database to authenticate against"`
 	Ssl                   bool   `default:"false" help:"Enable SSL"`
 	SslCertFile           string `default:"" help:"Path to the certificate file used to identify the local connection against MongoDB"`
 	SslCaCerts            string `default:"" help:"Path to the ca_certs file"`
@@ -40,7 +40,7 @@ func main() {
 	}
 
 	log.SetupLogging(args.Verbose)
-	// TODO validate arguments
+	validateArguments()
 
 	var wg sync.WaitGroup
 	collectorChan := startCollectorWorkerPool(10, &wg, mongoIntegration)
@@ -53,5 +53,32 @@ func main() {
 	wg.Wait()
 
 	mongoIntegration.Publish()
+
+}
+
+func validateArguments() {
+	if args.Username == "" {
+		log.Error("Must provide a username argument")
+		os.Exit(1)
+	}
+
+	if args.Password == "" {
+		log.Error("Must provide a password argument")
+		os.Exit(1)
+	}
+
+	if args.Host == "" {
+		log.Error("Must provide a host argument")
+		os.Exit(1)
+	}
+
+	if _, err := strconv.Atoi(args.Port); err != nil {
+		log.Error("Invalid port %s", args.Port)
+		os.Exit(1)
+	}
+
+	if args.SslInsecureSkipVerify {
+		log.Warn("Using insecure SSL. This connection is susceptible to man in the middle attacks.")
+	}
 
 }
