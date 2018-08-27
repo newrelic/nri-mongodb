449ef6e9483eb3fe481143dfc195e6f950b0aeed
Camden Cheek <camden.cheek@bluemedora.com>
2018-08-27 11:10:26 -0400

    Added CollectMetrics to mongod

--- src/entities/mongod.go
+++ src/entities/mongod.go
@@ -1,8 +1,14 @@
 package entities
 
 import (
+	"fmt"
+	"github.com/globalsign/mgo"
+	"github.com/newrelic/infra-integrations-sdk/data/metric"
 	"github.com/newrelic/infra-integrations-sdk/integration"
+	"github.com/newrelic/infra-integrations-sdk/log"
 	"github.com/newrelic/nri-mongodb/src/connection"
+	"github.com/newrelic/nri-mongodb/src/metrics"
+	"strings"
 )
 
 type MongodCollector struct {
@@ -13,8 +19,36 @@ func (c MongodCollector) GetEntity(i *integration.Integration) (*integration.Ent
 	return i.Entity(c.ConnectionInfo.Host, "mongod")
 }
 
+func (c MongodCollector) CollectMetrics(e *integration.Entity) {
+	session, err := c.ConnectionInfo.CreateSession()
+	if err != nil {
+		log.Error("Failed to connect to %s: %v", c.ConnectionInfo.Host, err)
+		return
+	}
+
+	ms := e.NewMetricSet("MongodSample",
+		metric.Attribute{Key: "displayName", Value: e.Metadata.Name},
+		metric.Attribute{Key: "entityName", Value: fmt.Sprintf("%s:%s", e.Metadata.Namespace, e.Metadata.Name)},
+	)
+
+	var isMaster metrics.IsMaster
+	err = session.Run(map[interface{}]interface{}{"isMaster": 1}, &isMaster)
+
+	ms.MarshalMetrics(isMaster)
+
+	if isMaster.SetName != "" {
+		collectReplSetMetrics(ms, c.ConnectionInfo, session)
+	}
+
+	var ss metrics.ServerStatus
+	session.Run(map[interface{}]interface{}{"serverStatus": 1}, &ss)
+
+	ms.MarshalMetrics(ss)
+
+}
+
 func GetMongods(shard *ShardCollector) ([]*MongodCollector, error) {
-	hostPorts := extractHostsFromReplicaSetString(shard.Host)
+	hostPorts, _ := parseReplicaSetString(shard.Host)
 
 	var mongodCollectors []*MongodCollector
 	for _, hostPort := range hostPorts {
@@ -29,4 +63,22 @@ func GetMongods(shard *ShardCollector) ([]*MongodCollector, error) {
 	}
 
 	return mongodCollectors, nil
+}
+
+func collectReplSetMetrics(ms *metric.Set, c *connection.ConnectionInfo, session *mgo.Session) error {
+
+	var replSetStatus metrics.ReplSetGetStatus
+	err := session.Run(map[interface{}]interface{}{"replSetGetStatus": 1}, &replSetStatus)
+	if err != nil {
+		return err
+	}
+
+	for _, host := range replSetStatus.Members {
+		if strings.HasPrefix(*host.Name, c.Host) {
+
+		}
+	}
+
+	return nil
+
 }
