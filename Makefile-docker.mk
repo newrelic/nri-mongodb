fa1098e3fff80b15edc6ce1851a7bdfb721d3f89
Corbin Phelps <corbin.phelps@bluemedora.com>
2018-09-13 09:56:08 -0400

    Added missing Makefiles, renamed config file

--- Makefile-docker.mk
+++ Makefile-docker.mk
@@ -8,4 +8,5 @@ docker-make:
 	@echo "=== $(INTEGRATION) === [ docker-fmt ]: Running make in Docker..."
 	@echo "Using Docker image $(DOCKER_IMAGE)"
 	@docker run -it --rm -v $(PWD):/go/src/github.com/newrelic/$(INTEGRATION_DIR) -w /go/src/github.com/newrelic/$(INTEGRATION_DIR) $(DOCKER_IMAGE) "make"
-	.PHONY: docker-fmt docker-make 
+	
+.PHONY: docker-fmt docker-make 
