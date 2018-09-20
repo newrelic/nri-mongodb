38553db40dc3e37c3108eafedd985c5e49e9247b
Camden Cheek <camden.cheek@bluemedora.com>
2018-09-20 09:05:02 -0400

    Change metrics to rates and add some

--- src/metrics/top.go
+++ src/metrics/top.go
@@ -20,54 +20,54 @@ type TopRecords struct {
 
 // TopRecordsTotal is a storage struct
 type TopRecordsTotal struct {
-	Time  *int `bson:"time" metric_name:"usage.totalInMilliseconds" source_type:"gauge"`
+	Time  *int `bson:"time" metric_name:"usage.totalInMillisecondsPerSecond" source_type:"rate"`
 	Count *int `bson:"count" metric_name:"usage.totalPerSecond" source_type:"rate"`
 }
 
 // TopRecordsReadLock is a storage struct
 type TopRecordsReadLock struct {
-	Time  *int `bson:"time" metric_name:"usage.readLockInMilliseconds" source_type:"gauge"`
+	Time  *int `bson:"time" metric_name:"usage.readLockInMillisecondsPerSecond" source_type:"rate"`
 	Count *int `bson:"count" metric_name:"usage.readLockPerSecond" source_type:"rate"`
 }
 
 // TopRecordsWriteLock is a storage struct
 type TopRecordsWriteLock struct {
-	Time  *int `bson:"time" metric_name:"usage.writeLockInMilliseconds" source_type:"gauge"`
+	Time  *int `bson:"time" metric_name:"usage.writeLockInMillisecondsPerSecond" source_type:"rate"`
 	Count *int `bson:"count" metric_name:"usage.writeLockPerSecond" source_type:"rate"`
 }
 
 // TopRecordsQueries is a storage struct
 type TopRecordsQueries struct {
-	Time  *int `bson:"time" metric_name:"usage.queriesInMilliseconds" source_type:"gauge"`
+	Time  *int `bson:"time" metric_name:"usage.queriesInMillisecondsPerSecond" source_type:"rate"`
 	Count *int `bson:"count" metric_name:"usage.queriesPerSecond" source_type:"rate"`
 }
 
 // TopRecordsGetmore is a storage struct
 type TopRecordsGetmore struct {
-	Time  *int `bson:"time" metric_name:"usage.getmoreInMilliseconds" source_type:"gauge"`
+	Time  *int `bson:"time" metric_name:"usage.getmoreInMillisecondsPerSecond" source_type:"rate"`
 	Count *int `bson:"count" metric_name:"usage.getmorePerSecond" source_type:"rate"`
 }
 
 // TopRecordsInsert is a storage struct
 type TopRecordsInsert struct {
-	Time  *int `bson:"time" metric_name:"usage.insertInMilliseconds" source_type:"gauge"`
+	Time  *int `bson:"time" metric_name:"usage.insertInMillisecondsPerSecond" source_type:"rate"`
 	Count *int `bson:"count" metric_name:"usage.insertPerSecond" source_type:"rate"`
 }
 
 // TopRecordsUpdate is a storage struct
 type TopRecordsUpdate struct {
-	Time  *int `bson:"time" metric_name:"usage.updateInMilliseconds" source_type:"gauge"`
+	Time  *int `bson:"time" metric_name:"usage.updateInMillisecondsPerSecond" source_type:"rate"`
 	Count *int `bson:"count" metric_name:"usage.updatePerSecond" source_type:"rate"`
 }
 
 // TopRecordsRemove is a storage struct
 type TopRecordsRemove struct {
-	Time  *int `bson:"time" metric_name:"usage.removeInMilliseconds" source_type:"gauge"`
+	Time  *int `bson:"time" metric_name:"usage.removeInMillisecondsPerSecond" source_type:"rate"`
 	Count *int `bson:"count" metric_name:"usage.removePerSecond" source_type:"rate"`
 }
 
 // TopRecordsCommands is a storage struct
 type TopRecordsCommands struct {
-	Time  *int `bson:"time" metric_name:"usage.commandsInMilliseconds" source_type:"gauge"`
+	Time  *int `bson:"time" metric_name:"usage.commandsInMillisecondsPerSecond" source_type:"rate"`
 	Count *int `bson:"count" metric_name:"usage.commandsPerSecond" source_type:"rate"`
 }
