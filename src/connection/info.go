79bfcf3fb5ecf1b0d7e9b887453ecdac4b0bbe71
Benedicto Tan <btan@newrelic.com>
2019-05-17 08:55:53 -0400

    implement support client certificate and  pvt key decryption

--- src/connection/info.go
+++ src/connection/info.go
@@ -3,9 +3,12 @@ package connection
 import (
 	"crypto/tls"
 	"crypto/x509"
+	"encoding/pem"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net"
+	"strings"
 	"time"
 
 	"github.com/globalsign/mgo"
@@ -22,6 +25,8 @@ type Info struct {
 	Port                  string
 	Ssl                   bool
 	SslCaCerts            string
+	PEMKeyFile            string
+	Passphrase            string
 	SslInsecureSkipVerify bool
 }
 
@@ -40,6 +45,8 @@ func (c *Info) clone(host, port string) *Info {
 		Port:                  port,
 		Ssl:                   c.Ssl,
 		SslCaCerts:            c.SslCaCerts,
+		PEMKeyFile:            c.PEMKeyFile,
+		Passphrase:            c.Passphrase,
 		SslInsecureSkipVerify: c.SslInsecureSkipVerify,
 	}
 	return info
@@ -94,14 +101,14 @@ func (c *Info) generateDialInfo() *mgo.DialInfo {
 	}
 
 	if c.Ssl {
-		addSSL(dialInfo, c.SslInsecureSkipVerify, c.SslCaCerts)
+		addSSL(dialInfo, c.SslInsecureSkipVerify, c.SslCaCerts, c.PEMKeyFile, c.Passphrase)
 	}
 
 	return dialInfo
 }
 
 // addSSL adds SSL to a dialInfo struct
-func addSSL(d *mgo.DialInfo, SslInsecureSkipVerify bool, SslCaCerts string) {
+func addSSL(d *mgo.DialInfo, SslInsecureSkipVerify bool, SslCaCerts string, pemKeyFile string, passPhrase string) {
 	tlsConfig := &tls.Config{
 		InsecureSkipVerify: SslInsecureSkipVerify,
 	}
@@ -119,9 +126,75 @@ func addSSL(d *mgo.DialInfo, SslInsecureSkipVerify bool, SslCaCerts string) {
 		}
 	}
 
+	if pemKeyFile != "" {
+
+		clientCert, err := parsePEMKeyFile(pemKeyFile, passPhrase)
+		if err != nil {
+			log.Error("Failed to open SSL PEM Key File: %v", err)
+		} else {
+			tlsConfig.Certificates = append([]tls.Certificate{}, clientCert)
+		}
+
+	}
+
 	// Use TLS to dial the server
 	d.DialServer = func(addr *mgo.ServerAddr) (net.Conn, error) {
 		conn, err := tls.Dial("tcp", addr.String(), tlsConfig)
 		return conn, err
 	}
+}
+
+func parsePEMKeyFile(pemKeyFile string, passphrase string) (tls.Certificate, error) {
+
+	keyPemData, err := ioutil.ReadFile(pemKeyFile)
+	if err != nil {
+		return tls.Certificate{}, err
+	}
+
+	var pvtKeyBlock, clientCertBlock *pem.Block
+	for {
+
+		block, rest := pem.Decode(keyPemData)
+		if block == nil {
+			return tls.Certificate{}, errors.New("not a valid PEM file")
+		}
+
+		if strings.Contains(block.Type, "PRIVATE KEY") {
+			pvtKeyBlock = block
+		}
+
+		if block.Type == "CERTIFICATE" {
+			clientCertBlock = block
+		}
+
+		if len(rest) == 0 || (clientCertBlock != nil && pvtKeyBlock != nil) {
+			break
+		}
+
+		keyPemData = rest
+	}
+
+	if clientCertBlock == nil {
+		return tls.Certificate{}, errors.New("no Client Certificate found in PEM key file")
+	}
+
+	if pvtKeyBlock == nil {
+		return tls.Certificate{}, errors.New("no Private Key found in PEM key file")
+	}
+
+	cert := tls.Certificate{}
+	if x509.IsEncryptedPEMBlock(pvtKeyBlock) {
+		decPemBytes, err := x509.DecryptPEMBlock(pvtKeyBlock, []byte(passphrase))
+		if err != nil {
+			return tls.Certificate{}, err
+		}
+
+		decBlock := pem.Block{}
+		decBlock.Bytes = decPemBytes
+		decBlock.Type = pvtKeyBlock.Type
+
+		return tls.X509KeyPair(pem.EncodeToMemory(clientCertBlock), pem.EncodeToMemory(&decBlock))
+	}
+
+	return cert, err
 }
