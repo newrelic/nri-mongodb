f5f9b71ddb5949e2d79a0013255dc0cbc677d0d5
Harsh Baste <hbaste@newrelic.com>
2022-07-14 10:27:55 +0800

    Added state checks (essentially assertions)

--- src/context.go
+++ src/context.go
@@ -20,23 +20,36 @@ type MongoContext struct {
 type Cmd = bson.D
 
 func (this *MongoContext) Disconnect() error {
+	if this.connection == nil {
+		return nil
+	}
 	if err := this.connection.Disconnect(context.TODO()); err != nil {
 		panic(err)
 	}
+	this.connection = nil
 	return nil
 }
 
 func (this *MongoContext) DB(dbname string) *MongoContext {
+	if this.connection == nil {
+		panic("DB needs a connection")
+	}
 	this.database = this.connection.Database(dbname)
 	return this
 }
 
 func (this *MongoContext) C(colname string) *MongoContext {
+	if this.database == nil {
+		panic("C needs a database")
+	}
 	this.collection = this.database.Collection(colname)
 	return this
 }
 
 func (this *MongoContext) FindAll(output interface{}) error {
+	if this.collection == nil {
+		panic("FindAll needs a collection")
+	}
 	cur, err := this.collection.Find(context.TODO(), bson.D{})
 	if err != nil {
 		return err
@@ -52,6 +65,9 @@ func (this *MongoContext) FindAll(output interface{}) error {
 }
 
 func (this *MongoContext) Run(command Cmd) string {
+	if this.database == nil {
+		panic("Run needs a database")
+	}
 	var result bson.M
 	err := this.database.RunCommand(context.TODO(), command).Decode(&result)
 	if err != nil {
@@ -66,6 +82,9 @@ func (this *MongoContext) Run(command Cmd) string {
 }
 
 func (this *MongoContext) RunUnmarshal(command Cmd, output interface{}) error {
+	if this.database == nil {
+		panic("RunUnmarshal needs a database")
+	}
 	var result bson.Raw
 	err := this.database.RunCommand(context.TODO(), command).Decode(&result)
 	if err != nil {
@@ -79,6 +98,9 @@ func (this *MongoContext) RunUnmarshal(command Cmd, output interface{}) error {
 }
 
 func (this *MongoContext) ListDatabases() []string {
+	if this.connection == nil {
+		panic("ListDatabases needs a database")
+	}
 	result, err := this.connection.ListDatabaseNames(context.TODO(), Cmd{})
 	if err != nil {
 		panic(err)
@@ -87,6 +109,9 @@ func (this *MongoContext) ListDatabases() []string {
 }
 
 func (this *MongoContext) CollectionNames() []string {
+	if this.database == nil {
+		panic("CollectionNames needs a database")
+	}
 	result, err := this.database.ListCollectionNames(context.TODO(), Cmd{})
 	if err != nil {
 		panic(err)
