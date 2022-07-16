9a9f5349222b5456abe4bede42abd6f168d3051f
Harsh Baste <hbaste@newrelic.com>
2022-07-16 12:39:28 +0800

    Completely eliminate globalsign/mgo driver

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/newrelic/nri-mongodb
 go 1.18
 
 require (
-	github.com/globalsign/mgo v0.0.0-20190517090918-73267e130ca1
 	github.com/newrelic/infra-integrations-sdk v3.7.3+incompatible
 	github.com/stretchr/testify v1.7.5
 	github.com/xeipuuv/gojsonschema v1.2.0
@@ -19,7 +18,7 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.4.0 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
-	github.com/xdg-go/scram v1.1.1 // indirect
+	github.com/xdg-go/scram v1.0.2 // indirect
 	github.com/xdg-go/stringprep v1.0.3 // indirect
 	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
--- src/connection/mongo_collection.go
+++ src/connection/mongo_collection.go
@@ -1,17 +1,17 @@
 package connection
 
-import "github.com/globalsign/mgo"
+// import "github.com/globalsign/mgo"
 
-// mongoCollection is a struct that allows shadowing of functions for mocking
-type mongoCollection struct {
-	col *mgo.Collection
-}
+// // mongoCollection is a struct that allows shadowing of functions for mocking
+// type mongoCollection struct {
+// 	col *mgo.Collection
+// }
 
-// FindAll marshals all items in the collection into result
-func (c *mongoCollection) FindAll(result interface{}) error {
-	return c.col.Find(nil).All(result)
-}
+// // FindAll marshals all items in the collection into result
+// func (c *mongoCollection) FindAll(result interface{}) error {
+// 	return c.col.Find(nil).All(result)
+// }
 
-func (c *mongoCollection) PipeAll(request, result interface{}) error {
-	return c.col.Pipe(request).All(result)
-}
+// func (c *mongoCollection) PipeAll(request, result interface{}) error {
+// 	return c.col.Pipe(request).All(result)
+// }
